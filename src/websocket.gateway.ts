0bed4d98ea830ae5e8a3a64e292d66518784c6fa
tanabe1422 <1422hal@gmail.com>
2021-04-02 23:38:23 +0900

    プロジェクト生成

---
+++ src/websocket.gateway.ts
@@ -0,0 +1,29 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class WebsocketGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: any): string {
+    return 'Hello world!';
+  }
+
+  // 接続時の処理
+  handleConnection(client: Socket) {
+    console.log(`connect: ${client.id}`);
+    // 恐らくcookie取り出せる。パースされてない。
+    // client.handshake.headers.cookie;
+  }
+
+  // 切断時の処理
+  handleDisconnect(client: Socket) {
+    console.log(`dissconnect: ${client.id}`);
+  }
+}
